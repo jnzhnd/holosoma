c27370f0201b1935a3818a6b30423b7fa5a6dcd0
Tomasz Lewicki <jtomasle@amazon.com>
2026-03-27 14:02:08 -0700

    Remove unused

--- src/holosoma_inference/holosoma_inference/inputs/api/base.py
+++ src/holosoma_inference/holosoma_inference/inputs/api/base.py
@@ -3,12 +3,8 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING
 
 from holosoma_inference.inputs.api.commands import StateCommand, VelocityCommand
-
-if TYPE_CHECKING:
-    from holosoma_inference.policies.base import BasePolicy
 
 
 class VelocityInput(ABC):
